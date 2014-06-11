23cefe9c3c9d9d8ee1c2dbd8502454b8b60840a8
EndlessCheng <loli.con@qq.com>
2014-06-11 21:28:51 +0800

    Update SimpleOutputStream.java

--- share/classes/mjava/io/SimpleOutputStream.java
+++ share/classes/mjava/io/SimpleOutputStream.java
@@ -16,11 +16,6 @@ public class SimpleOutputStream {
 
 	public static void run() {
 		char[] s = { 'H', 'e', 'l', 'l', 'o' };
-		System.out.println(new String(s));
-		
-		// 下面这行代码总是不显示东西，why？
-		//new SimpleOutputStream().write(new MString(s)); 
-		
-		
+		new SimpleOutputStream().write(new MString(s));
 	}
 }
