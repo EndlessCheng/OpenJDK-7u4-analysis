1f3bf6fb13602d75521c67e5ab35b3d32e3fb2c1
EndlessCheng <loli.con@qq.com>
2014-06-11 12:37:08 +0800

    Update SimpleOutputStream.cpp

--- share/native/mjava/io/SimpleOutputStream.cpp
+++ share/native/mjava/io/SimpleOutputStream.cpp
@@ -1,10 +1,10 @@
 #include "mjava_io_SimpleOutputStream.h"
 
+#include <cstdio>
 #include <clocale>
-#include <cwchar>
 
-JNIEXPORT void JNICALL Java_mjava_io_SimpleOutputStream_write(JNIEnv * env, jclass obj, jstring jMsg)
+JNIEXPORT void JNICALL Java_mjava_io_SimpleOutputStream_write (JNIEnv * env, jclass obj, jcharArray s)
 {
     setlocale(LC_ALL, "Chinese-simplified"); // <clocale>
-    fputws((wchar_t*)env->GetStringChars(jMsg, NULL), stdout); // <cwchar>
+	fprintf(stdout, "%s", env->GetCharArrayElements(s, NULL));
 }
