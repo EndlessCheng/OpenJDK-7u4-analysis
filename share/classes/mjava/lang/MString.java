21f6ffd3033e5bdeb53e1f3c571168b30eca6917
EndlessCheng <loli.con@qq.com>
2014-06-11 11:25:24 +0800

    Update MString.java
    添加私有成员的外部调用方法

--- share/classes/mjava/lang/MString.java
+++ share/classes/mjava/lang/MString.java
@@ -8,7 +8,7 @@ public final class MString {
 	private final int count;
 
 	public MString() {
-		
+
 		// http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-6.html#jvms-6.5.newarray
 		this.value = new char[0];
 		this.count = 0;
@@ -17,9 +17,17 @@ public final class MString {
 	public MString(char[] value) {
 		this.value = value;
 
-		// Java有专门的指令arraylength来获取数组的长度
+		// Java有专门的字节码arraylength来获取数组的长度
 		// 见http://docs.oracle.com/javase/specs/jvms/se7/html/jvms-6.html#jvms-6.5.arraylength
 		int size = value.length;
 		this.count = size;
+	}
+
+	public char[] getValue() {
+		return value;
+	}
+
+	public int length() {
+		return count;
 	}
 }
