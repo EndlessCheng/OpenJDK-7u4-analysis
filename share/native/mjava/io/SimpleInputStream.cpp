c241e731be0176a36c97ab48eeacab443e3e17d1
EndlessCheng <loli.con@qq.com>
2014-06-11 20:59:01 +0800

    Update SimpleInputStream.cpp
    修改返回值为jcharArray，与MString相匹配。

--- share/native/mjava/io/SimpleInputStream.cpp
+++ share/native/mjava/io/SimpleInputStream.cpp
@@ -8,17 +8,25 @@ const int SZ = 1024;
 JNIEXPORT jboolean JNICALL Java_mjava_io_SimpleInputStream_hasnextLine (JNIEnv * env, jobject obj)
 {
 	setlocale(LC_ALL, "Chinese-simplified"); // <clocale>
+
 	wchar_t ch = getwchar();
 	if(ch == WEOF) return 0; // 不要用EOF
 	//if(ch == L'E') return 0; // 测试：读到E开头的字符串就退出
+
 	ungetwc(ch, stdin);
 	return 1;
 }
 
-JNIEXPORT jstring JNICALL Java_mjava_io_SimpleInputStream_readLine (JNIEnv * env, jobject obj)
+JNIEXPORT jcharArray JNICALL Java_mjava_io_SimpleInputStream_readLine (JNIEnv * env, jobject obj)
 {
 	setlocale(LC_ALL, "Chinese-simplified"); // <clocale>
+
 	wchar_t ws[SZ];
 	fgetws(ws, SZ, stdin);
-	return env->NewString((jchar*)ws, wcslen(ws));
+	jsize len = wcslen(ws);
+
+	jcharArray jcArr = env->NewCharArray(len); // 初始化jcharArray，这一步是必须的
+	env->SetCharArrayRegion(jcArr, 0, len, (jchar*)ws);
+
+	return jcArr;
 }
